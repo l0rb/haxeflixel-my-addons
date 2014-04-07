60310cb4683cdf3a1fe2c6350d62ef4f32789742
l0rb <lorbritzer@yahoo.de>
2014-04-07 17:05:58 +0200

    MyMessage: valign and small bug

--- flixel/addons/ui/MyMessage.hx
+++ flixel/addons/ui/MyMessage.hx
@@ -3,44 +3,73 @@ package flixel.addons.ui;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
-import flixel.plugin.MouseEventManager;
 import flixel.addons.text.MyText;
 
 class MyMessageState extends FlxSubState {
+   public static var VALIGN_TOP= 1;
+   public static var VALIGN_CENTER= 2;
+   public static var VALIGN_BOTTOM= 3;
+
    public var bgcolor(default,set):Int;
 
    private var _bg:FlxSprite;
+   private var _message:Array<String>;
+   private var _index:Int;
+   private var _valign:Int;
+   private var padding:Int;
+   private var margin:Int;
+   private var _text:MyText;
 
-   public function new(m:String,Bgcolor=0xBB000000,TextSize=18) {
+   private function update_y() {
+      _text.draw();
+      if(_valign==VALIGN_TOP) _text.y= margin+padding;
+      if(_valign==VALIGN_BOTTOM) _text.y= FlxG.height-_text.frameHeight-margin-padding;
+      if(_valign==VALIGN_CENTER) _text.y= (FlxG.height-_text.frameHeight)/2+padding;
+      trace(_text,_text.text);
+      _bg.y= _text.y-padding;
+      _bg.makeGraphic(_text.frameWidth+2*padding,_text.frameHeight+2*padding,bgcolor,true);
+   }
+
+   public function new(Message:Array<String>,TextSize=18,Valign=2,Bgcolor=0xBB000000,P=5,M=10) {
       super();
-      var padding= 5;
-      var margin= 10;
+      padding= P;
+      margin= M;
 
-      var g= new FlxSprite(0,0).makeGraphic(FlxG.width,FlxG.height,0x01000000,true);
-      MouseEventManager.add(g,null,unfreeze);
-      add(g);
+      _message= Message;
+      _index= 0;
+      _valign= Valign;
 
       var from_top= Std.int(FlxG.height/2);
       
-      var t= new MyText(margin+padding,from_top+padding,FlxG.width-2*(margin+padding),m,TextSize);
-      //t.alignment= "center";
-      //t.borderStyle= FlxText.BORDER_OUTLINE;
-      //t.borderColor= 0; // that's black
-      t.y-= t.frameHeight/2;
+      _text= new MyText(margin+padding,from_top+padding,FlxG.width-2*(margin+padding),_message[0],TextSize);
+      
+      //_text.alignment= "center";
+      //_text.borderStyle= FlxText.BORDER_OUTLINE;
+      //_text.borderColor= 0; // that's black
          
-      _bg= new FlxSprite(t.x-padding,t.y-padding);
-      _bg.makeGraphic(t.frameWidth+2*padding,t.frameHeight+2*padding,Bgcolor,true);
+      _bg= new FlxSprite(_text.x-padding,_text.y-padding);
+      update_y();
+
       add(_bg);
-      add(t);
+      add(_text);
       
       bgcolor= Bgcolor;
    }
-   private function unfreeze(s:FlxSprite) {
-      close();
+   private function next() {
+      _index+= 1;
+      if(_index>=_message.length) close();
+      else {
+         _text.text= _message[_index];
+         update_y();
+      }
    }
    public function set_bgcolor(color:Int):Int {
       _bg.makeGraphic(_bg.frameWidth,_bg.frameHeight,color,true);
       return bgcolor= color;
+   }
+   override public function update() {
+      super.update();
+      if(FlxG.mouse.justReleased) next();
    }
 
 }
