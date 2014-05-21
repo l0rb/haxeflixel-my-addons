8634b75b0a841aad07e3139a4abc17a92ec6ff2f
l0rb <lorbritzer@yahoo.de>
2014-05-21 22:53:15 +0200

    multidimensional Arrays

---
+++ flixel/addons/util/Arrays.hx
@@ -0,0 +1,27 @@
+package flixel.addons.util;
+
+abstract Array2D<T>(Array<Array<T>>) {
+   public function new() {
+      this= new Array<Array<T>>();
+   }
+   @:arrayAccess public inline function arrayAccess(key:Int):Dynamic {
+      if(this[key]==null) this[key]= new Array<T>();
+      return this[key];
+   }
+}
+
+abstract Array3D<T>(Array<Array2D<T>>) {
+   public function new() {
+      this= new Array<Array2D<T>>();
+   }
+   @:arrayAccess public inline function arrayAccess(key:Int):Array2D<T> {
+      if(this[key]==null) this[key]= new Array2D<T>();
+      return this[key];
+   }
+   @:arrayAccess public inline function arrayWrite<T>(key:Int, value:Array2D<T>):Array2D<T> {
+        //Reflect.setField(this, key, value);
+        this[key]= value;
+        return value;
+    }
+}
+
