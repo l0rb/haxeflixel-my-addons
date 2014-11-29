1c18a34bde7deeccc8b7e171c3485cc49c953a0f
l0rb <lorbritzer@yahoo.de>
2014-11-29 02:52:00 +0100

    complex numbers square

--- flixel/addons/math/MyComplex.hx
+++ flixel/addons/math/MyComplex.hx
@@ -23,6 +23,13 @@ abstract MyComplex(MyComplexNumberBaseClass) {
       return Math.sqrt(Math.pow(this.real,2)+Math.pow(this.imaginary,2));
    }
 
+   public var square(get, never):MyComplex;
+   public function get_square():MyComplex {
+      var a= Math.pow(this.real,2)-Math.pow(this.imaginary,2);
+      var b= 2*this.real*this.imaginary;
+      return new MyComplex(a,b);
+   }
+
    @:op(a+b) public function addition(rhs:MyComplex) {
       return new MyComplex(this.real+rhs.real,this.imaginary+rhs.imaginary);
    }
